5e65afbae5f14b4f5d42f0c47750fd6316c3bd4b
Askari <blgntobi@gmail.com>
2020-06-11 23:51:01 +0100

    created actions

---
+++ src/actions/questions.js
@@ -0,0 +1,57 @@
+import { showLoading, hideLoading } from "react-redux-loading"
+import { saveQuestion, saveQuestionAnswer } from "../utils/api"
+
+
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_QUESTION ='ADD_QUESTION'
+export const SAVE_QUESTION_ANSWER = 'SAVE_QUESTION_ANSWER'
+
+
+export function receiveQuestions(questions){
+    return {
+        type: RECEIVE_QUESTIONS,
+        questions
+    }
+}
+
+function addQuestion (question) {
+    return {
+        type: ADD_QUESTION,
+        question
+    }
+}
+
+
+export function handleAddQuestion (question){
+    return (dispatch) => {
+        
+
+        dispatch(showLoading())
+        return saveQuestion (question)
+        .then((question) => dispatch(addQuestion(question)))
+        .then(() => dispatch(hideLoading()))
+    }
+}
+
+function saveAnswer({authedUser, qid, answer}){
+    return {
+        type: SAVE_QUESTION_ANSWER,
+        authedUser,
+        qid,
+        answer
+
+    }
+}
+
+export function handleSaveQuestionAnswer(info){
+    return (dispatch) => {
+
+        dispatch(saveAnswer(info))
+        return saveQuestionAnswer(info)
+        .catch((e) => {
+            console.warn("Error in handleSaveQuestionAnswer: ", e)
+            dispatch(saveAnswer(info))
+            alert("There was an error saving your choice. Try again")
+        })
+    }
+}
